3623ddb16fed271bb8f6c08ea239e1a501940587
Rama Sai Kiran Medam <146340093+Ramasaikiran@users.noreply.github.com>
2025-12-20 11:46:18 +0530

    Time: 8 ms (74.40%) | Memory: 47.3 MB (13.28%) - LeetSync

---
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        if(strs==null || strs.length==0) return 0;
+        int rows=strs.length;
+        int col=strs[0].length();
+        int count=0;
+        for(int i=0;i<col;i++){
+            for(int j=0;j<rows-1;j++){
+            if(strs[j].charAt(i)>strs[j+1].charAt(i)){
+                count++;
+                break;
+            }
+        }
+        }
+        return count;
+    }
+}
