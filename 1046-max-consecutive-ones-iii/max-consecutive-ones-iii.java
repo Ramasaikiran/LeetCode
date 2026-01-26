7bbce3024bc7f31884ee0ff28be204ed8e5cb198
Rama Sai Kiran Medam <146340093+Ramasaikiran@users.noreply.github.com>
2026-01-26 19:42:13 +0530

    Time: 4 ms (48.48%) | Memory: 52.2 MB (45.40%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -3,7 +3,7 @@ class Solution {
         int l=0,r=0,maxlen=0,count=0,len=0;
         while(r<nums.length){
             if(nums[r]==0) count++;
-            while(count>k){
+            if(count>k){
                 if(nums[l]==0){
                     count--;
                 }
