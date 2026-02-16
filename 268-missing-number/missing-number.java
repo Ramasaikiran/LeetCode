b63b77fb7be2d7bf52fc17df43a6cf3a64be0a12
Rama Sai Kiran Medam <146340093+Ramasaikiran@users.noreply.github.com>
2026-02-16 16:41:51 +0530

    Time: 5 ms (21.24%) | Memory: 47.3 MB (65.97%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        for(int num:nums){
+            hm.put(num,1);
+        }
+        for(int i=0;i<=nums.length;i++){
+            if(!hm.containsKey(i)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
